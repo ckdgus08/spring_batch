0c0e73753fe54d12a812dc62ab2f4dd9b49ca25e
ckdgus08 <ckdgus0808@naver.com>
2021-05-22 22:13:34 +0900

    feat: CallableTaskletAdapter 사용하여 tasklet 구현

--- src/main/java/com/github/ckdgus08/SpringBatchApplication.java
+++ src/main/java/com/github/ckdgus08/SpringBatchApplication.java
@@ -6,14 +6,13 @@ import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
-import org.springframework.batch.core.job.CompositeJobParametersValidator;
-import org.springframework.batch.core.job.DefaultJobParametersValidator;
-import org.springframework.batch.core.step.tasklet.Tasklet;
+import org.springframework.batch.core.step.tasklet.CallableTaskletAdapter;
+import org.springframework.batch.repeat.RepeatStatus;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
-import java.util.Arrays;
+import java.util.concurrent.Callable;
 
 @EnableBatchProcessing
 @SpringBootApplication
@@ -27,64 +26,37 @@ public class SpringBatchApplication {
     public static void main(String[] args) {
         SpringApplication.run(SpringBatchApplication.class, args);
     }
-//
-//    @Bean
-//    public Step step1() {
-//        return this.stepBuilderFactory.get("step1")
-//                .tasklet(new HelloWorld())
-//                .listener(promotionListener())
-//                .build();
-//    }
 
     @Bean
-    public Step step2() {
-        return this.stepBuilderFactory.get("step2")
-                .tasklet(HelloWorldTasklet())
+    public Job callableJob() {
+        return this.jobBuilderFactory.get("callableJob")
+                .start(callableStep())
                 .build();
     }
 
     @Bean
-    public Job job() {
-        return this.jobBuilderFactory.get("job")
-                .start(step2())
-//                .next(step2())
-//                .validator(validator())
-//                .incrementer(new DailyJobTimeStamper())
-//                .listener(JobListenerFactoryBean.getListener(
-//                        new JobLoggerListener()))
+    public Step callableStep() {
+        return this.stepBuilderFactory.get("callableStep")
+                .tasklet(tasklet())
                 .build();
     }
-//
-//    @Bean
-//    public StepExecutionListener promotionListener() {
-//        ExecutionContextPromotionListener listener = new ExecutionContextPromotionListener();
-//
-//        listener.setKeys(new String[]{"name"});
-//        return listener;
-//    }
 
     @Bean
-    public Tasklet HelloWorldTasklet() {
-        return new HelloWorld();
+    public Callable<RepeatStatus> callableObject() {
+        return () -> {
+            System.out.println("This was Executed in another thread");
+            return RepeatStatus.FINISHED;
+        };
     }
 
     @Bean
-    public CompositeJobParametersValidator validator() {
-        CompositeJobParametersValidator validator =
-                new CompositeJobParametersValidator();
+    public CallableTaskletAdapter tasklet() {
+        CallableTaskletAdapter callableTaskletAdapter = new CallableTaskletAdapter();
 
-        DefaultJobParametersValidator defaultJobParametersValidator
-                = new DefaultJobParametersValidator(
-                new String[]{"fileName"},
-                new String[]{"name", "currentDate"});
+        callableTaskletAdapter.setCallable(callableObject());
 
-        defaultJobParametersValidator.afterPropertiesSet();
-
-        validator.setValidators(
-                Arrays.asList(new ParameterValidator(),
-                        defaultJobParametersValidator));
-
-        return validator;
+        return callableTaskletAdapter;
     }
+
 
 }
